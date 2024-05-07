43b1606f3d9206d0764627cfdc7fcb94524ff3e1
Petros Kitsaras <13155066+TheGamerUrso@users.noreply.github.com>
2024-05-07 13:51:31 +0200

    Initial Project Add\

---
+++ src/main/java/thegamerurso/springframework/tgupetclinic/TguPetClinicApplication.java
@@ -0,0 +1,13 @@
+package thegamerurso.springframework.tgupetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TguPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TguPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thegamerurso/springframework/tgupetclinic/TguPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package thegamerurso.springframework.tgupetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TguPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
