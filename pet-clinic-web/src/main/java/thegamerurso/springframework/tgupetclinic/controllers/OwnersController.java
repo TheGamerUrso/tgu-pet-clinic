966884550ed0f21d7d90e9fdd24bf4fc97ebdb18
Petros Kitsaras <13155066+TheGamerUrso@users.noreply.github.com>
2024-06-05 15:05:19 +0200

    adding owners page and controller . closes #13

---
+++ pet-clinic-web/src/main/java/thegamerurso/springframework/tgupetclinic/controllers/OwnersController.java
@@ -0,0 +1,14 @@
+package thegamerurso.springframework.tgupetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnersController
+{
+    @RequestMapping({"" ,"/","/index","/index.html"})
+    public String ListOwners(){
+        return "owners/index";
+    }
+}
