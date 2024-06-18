168e61bc547222f0b7c2fa63deb07135d6181b85
Petros Kitsaras <petros.kitsaras@gmail.com>
2024-06-18 15:17:40 +0200

    Fixed MasterLayout , added PetTypeService closes #29

---
+++ pet-clinic-data/src/main/java/thegamerurso/springframework/tgupetclinic/services/map/PetTypeService.java
@@ -0,0 +1,10 @@
+package thegamerurso.springframework.tgupetclinic.services.map;
+
+import thegamerurso.springframework.tgupetclinic.model.PetType;
+import thegamerurso.springframework.tgupetclinic.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType,Long>
+{
+
+}
+
