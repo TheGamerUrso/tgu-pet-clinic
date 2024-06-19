63bee34ff6e5601407fe77d87354ce7e61a9be8b
Petros Kitsaras <petros.kitsaras@gmail.com>
2024-06-19 16:30:00 +0200

    Fixed broken links. closes #45

--- pet-clinic-web/src/main/java/thegamerurso/springframework/tgupetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/thegamerurso/springframework/tgupetclinic/controllers/OwnerController.java
@@ -5,6 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import thegamerurso.springframework.tgupetclinic.services.OwnerService;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RequestMapping("/owners")
 @Controller
@@ -23,4 +26,10 @@ public class OwnerController
 
         return "owners/index";
     }
+
+    @RequestMapping("/find")
+    public String findOwners() {
+        return "notImplemented";
+    }
+    
 }
