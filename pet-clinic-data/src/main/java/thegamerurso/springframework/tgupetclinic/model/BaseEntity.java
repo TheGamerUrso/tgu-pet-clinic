e4de86ccd21cf6e820e6d9a8c32b39c6b6456bfd
Petros Kitsaras <petros.kitsaras@gmail.com>
2024-07-02 13:42:21 +0200

    Updated base entity with JPA annotions. closes #35

--- pet-clinic-data/src/main/java/thegamerurso/springframework/tgupetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/thegamerurso/springframework/tgupetclinic/model/BaseEntity.java
@@ -2,8 +2,16 @@ package thegamerurso.springframework.tgupetclinic.model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
