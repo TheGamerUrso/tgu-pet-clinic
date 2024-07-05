bd805edf81162b445328ecbdbd6e5a23d8127d8a
Petros Kitsaras <petros.kitsaras@gmail.com>
2024-07-05 13:32:45 +0200

    added spring data jpa service for owners. closes #40

--- pet-clinic-data/src/main/java/thegamerurso/springframework/tgupetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/thegamerurso/springframework/tgupetclinic/repositories/OwnerRepository.java
@@ -3,8 +3,11 @@ package thegamerurso.springframework.tgupetclinic.repositories;
 import org.springframework.data.repository.CrudRepository;
 
 import thegamerurso.springframework.tgupetclinic.model.Owner;
+import java.util.List;
+
 
 public interface OwnerRepository extends CrudRepository<Owner,Long>
 {
-
+    Owner findByLastName(String lastName);
+    
 }
